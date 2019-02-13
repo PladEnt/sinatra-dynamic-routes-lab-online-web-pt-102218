f6403daa5d794cc70efe1e3c61ee189b0c8929ab
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-13 21:20:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,28 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  
+  get "/reversename/:name" do
+    params[:name].reverse
+  end
+  
+  get "/square/:number" do
+    (params[:number].to_i**2).to_s
+  end
+  
+  get "/say/:number/:phrase" do
+    string = ""
+    
+    (params[:number].to_i).times do
+      string += "#{params[:phrase]}\n"
+    end
+    string
+  end
+  
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    string = params[:word1] + params[:word2] + params[:word3] +  params[:word4] + params[:word5]
+    string
+  end
 end
+
+
